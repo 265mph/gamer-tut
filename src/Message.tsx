eabfec40d3da6391c3b66cf03a2fb26693d67a90
Aina Temiloluwa Samuel <samuelaina18@gmail.com>
2026-08-18 10:59:07 +0100

    useState and Props

---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+    // JSX XML syntax - head over to babeljs.io/repl
+    return <h1>Hello World</h1>
+}
+
+export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+interface Props {
+    items: string[],
+    header: string
+}
+
+function ListGroup({items, header}: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1)
+
+  return (
+    <>
+      <h1>{header}</h1>
+      {items.length === 0 && <p>no items found</p>}
+
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {setSelectedIndex(index)}}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
